6b9fa7514def5619445bcd269620a78dc80c9715
tkirtman <kirtmata@mail.uc.edu>
2026-06-06 18:03:01 -0400

    added pseudo code comments

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -5,14 +5,17 @@ void main() {
     // Code a logic program that asks the user to enter their age
     // and then if they are 21 or over displays a message that they get a wrist band.
     //Note that the program does nothing if they are not 21 or over…
-
+// set variable
     int userAge = 0;
+// create scanner
     Scanner in = new Scanner(System.in);
-
+//print prompt
     System.out.println("Please enter your age: ");
-    if(in.hasNextInt()){
+//check if int and then check the inpute age
+   if(in.hasNextInt()){
         userAge = in.nextInt();
         if(userAge >= 21){
+// print output
             System.out.println("You get a wristband.");
         }
     }
